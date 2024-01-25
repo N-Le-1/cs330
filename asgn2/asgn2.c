b858dab12f041a832bfa1be928dd128df59453d6
N-Le-1 <156348689+N-Le-1@users.noreply.github.com>
2024-01-25 19:08:08 +0000

    update asgn2

--- asgn2/asgn2.c
+++ asgn2/asgn2.c
@@ -3,20 +3,47 @@
 #include "asgn2.h"
 
 int main(){
+    //initialize variables
+    int n, n2, n3 = 0;
+    float x,y = 0;
+
     //Q. 1
-    cubeOfOdd();
+    printf("***Question 1: cubeOfOdd \n");
+    printf("Enter an integer value: \n");
+    scanf("%d",&n);
+    cubeOfOdd(n);
 
     //Q.2
-    introToCS330();
+    printf("***Question 2: introToCS330 \n");
+    printf("Enter an integer value: \n");
+    scanf("%d",&n);
+    introToCS330(n);
 
     //Q.3
-    printHELLO();
+    printf("***Question 3: printHELLO*** \n");
+    printf("Enter an integer value: \n");
+    scanf("%d",&n);
+    printHELLO(n);
 
     //Q.4
-    paintGallons();
+    printf("***Question 4: paintGallons*** \n");
+    printf("Enter the length of the wall: \n");
+    scanf("%d",&n);
+    printf("Enter the width of the wall: \n");
+    scanf("%d",&n2);
+    printf("Enter the height of the wall: \n");
+    scanf("%d",&n3);
+    paintGallons(n,n2,n3);
 
     //Q.5
-    grader(100,85,21);
+    printf("***Question 5: grader*** \n");
+    printf("Enter your average exam grade: \n");
+    scanf("%f",&x);
+    printf("Enter your average homework grade: \n");
+    scanf("%f",&y);
+    printf("Enter your attendance: \n");
+    scanf("%d",&n);
+    grader(x,y,n);
     
     return 0;
     
